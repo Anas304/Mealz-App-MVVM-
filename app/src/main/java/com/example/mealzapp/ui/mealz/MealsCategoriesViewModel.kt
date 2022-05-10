05d57651d60c6b96d647692d7b154bc29184ef75
Anas Ahmad <anasfarhad304@gmail.com>
2022-05-10 08:41:53 +0500

    testing Logs commit

--- app/src/main/java/com/example/mealzapp/ui/mealz/MealsCategoriesViewModel.kt
+++ app/src/main/java/com/example/mealzapp/ui/mealz/MealsCategoriesViewModel.kt
@@ -1,10 +1,13 @@
 package com.example.mealzapp.ui.mealz
 
+import android.util.Log
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.mealzapp.model.MealsRepo
 import com.example.mealzapp.model.response.MealsCategoriesResponse
 import com.example.mealzapp.model.response.MealsSingleObjectResponse
@@ -15,24 +18,20 @@ import kotlinx.coroutines.launch
 
 class MealsCategoriesViewModel(private val repository: MealsRepo = MealsRepo()) : ViewModel() {
 
-    private val mealsJob = Job()
 
     init {
-        val scope = CoroutineScope(mealsJob + Dispatchers.IO)
-        scope.launch {
+        Log.d("TAG_Coroutine","We are about to launch a coroutine")
+        viewModelScope.launch(Dispatchers.IO) {
+            Log.d("TAG_Coroutine","We have launched the coroutine")
             val meals = getMealsResponse()
+            Log.d("TAG_Coroutine","We have received the async data")
             mealsState.value = meals
         }
+        Log.d("TAG_Coroutine","Other Work!!")
     }
 
     val mealsState: MutableState<List<MealsSingleObjectResponse>> =
         mutableStateOf(emptyList<MealsSingleObjectResponse>())
-
-
-    override fun onCleared() {
-        super.onCleared()
-        mealsJob.cancel()
-    }
 
     private suspend fun getMealsResponse(): List<MealsSingleObjectResponse> {
         return repository.getMealsResponse().categories
